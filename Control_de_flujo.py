8a2fcc17ed2b17fa0bef14c2898d9aba65ea913c
Jonathan Mulero <jhonyganehd@gmail.com>
2024-12-03 10:23:31 +0100

    Control de flujos con if else

---
+++ Control_de_flujo.py
@@ -0,0 +1,32 @@
+"""EJEMPLOS"""
+
+mascota = "gato"
+
+if mascota == "perro":
+    print("Tienes un perro")
+elif mascota == 'gato':
+    print("Tienes un gato")
+else:
+    print("No sé que mascota tienes")
+
+
+edad = 16
+calificacion = 9
+
+if edad < 18:
+    print('Eres menor de edad')
+    if calificacion >= 7:
+        print('Aprobado')
+    else:
+        print('No has aprobado')
+else:
+    print('Eres adulto')
+
+
+
+
+
+
+
+
+
